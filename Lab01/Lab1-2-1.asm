b5490e1f31abc09181ee0cb58ecfd260598c1d25
ZanochkinEgor <egor.zanochkin9@gmail.com>
2020-10-13 15:39:06 +0300

    Lab01 upd2

--- Lab01/Lab1-2-1.asm
+++ Lab01/Lab1-2-1.asm
@@ -1,5 +1,6 @@
-include \masm64\include64\masm64rt.inc ; библиотеки
+include \masm64\include64\masm64rt.inc  ; библиотеки
 count PROTO arg_a:QWORD,arg_b:QWORD,arg_c:QWORD,arg_d:QWORD, arg_e:QWORD,arg_f:QWORD,arg_g:QWORD
+
 .data
 _a1 dq 5    ; аргумент а
 _b1 dq 2    ; аргумент b
@@ -37,6 +38,7 @@ mov _res1, r10  ; заносим результат в _res1
 ret
 count endp
 entry_point proc
+
 invoke count,_a1,_b1,_c1,_d1,_e1,_f1,_g1
 invoke wsprintf,ADDR buf1,ADDR _text,_res1,ADDR _res1
 invoke MessageBox,0,addr buf1, addr _title, MB_ICONINFORMATION
